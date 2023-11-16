a2b0643ad8f0f76244bb898365c03a187fef5804
Egy-Team-Dev <elhelwahmed4@gmail.com>
2023-11-16 07:21:30 +0200

    start on Pofile page

---
+++ src/components/shared/ThreadsTab.tsx
@@ -0,0 +1,12 @@
+type Props = {
+  currentUserId: string;
+  accountId: string;
+  accountTypeId: string;
+};
+
+const ThreadsTab = ({ currentUserId, accountId, accountTypeId }: Props) => {
+  // TODO: Fetch Profile Threads
+  return <div>ThreadsTab</div>;
+};
+
+export default ThreadsTab;
